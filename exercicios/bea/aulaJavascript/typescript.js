6f4c3b0ac2bec80ddfd90434e1da083e962a36e0
Beatriz Brito <bihsl67@gmail.com>
2023-08-02 20:02:47 -0300

    npm

---
+++ exercicios/bea/aulaJavascript/typescript.js
@@ -0,0 +1,38 @@
+//arvore dom: pra ter uma otimização da busca, organização, pastas etc, é ideal organizar em modo de arvore ou grafo 
+
+//linguagens que tem acesso à essa árvore (como o js) consegue manipular em tempo real os elementos de um site:
+    // - cores
+    // - texto, etc
+
+//js deixou tudo dinâmico e reativo
+
+//sintaxe declarativa SOMENTE EM JS (exemplo: ferramentas para upload de imagens com visual bacana)
+    //a declarativa tende a ser um código menor, sem selecionar um id e toda aquela referência gigantesca, é como se puxasse um bloco da arvore direto.
+//sintaxe IMPERATIVA (pesquisar) exemplo: documentGetById
+
+//o TYPESCRIPT é um COMPILADOR DO JAVASCRIPT
+    //estudar o que é um NPM {ferramenta para baixar as bibliotecas, como se fosse um uTorrent, ele serve pra baixar arquivos toorrents}
+        //serve para identificar 
+        
+        //.json serve para? (objeto) packge.json identifica os pacotes, bibliotecas, etc.
+
+        //node_modulos é a pasta mais pesada.
+
+    //baixar o npm, e em seguida o typescript, criar um arquivo index.ts
+        //testar:
+
+
+
+    //javascript
+    const programa = {
+        sayHello() {
+            console.log["Hello Word"];
+            return 1;
+        },
+    } 
+
+    //typescript
+//exemplo do professor lista de humanos: o ts praticamente serviu pra não deixar compilar no caso da lista não ter todos os tipos da lista Humanos.
+
+
+//PRIMEIRA AULA QUE BOIEI LEGAL KK

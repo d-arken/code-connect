4f5b754eb37fefdace57f289123b7a564f4d2c9d
Beatriz Brito <bihsl67@gmail.com>
2023-08-07 14:56:02 -0300

    a

---
+++ exercicios/bea/carrinho-de-compras/deuruim/deuruim.js
@@ -0,0 +1,17 @@
+function carrinhoProdutos () {
+    var produtosCarrinho = {
+        barbie1: {
+            imagem: "https://m.media-amazon.com/images/I/91oTL4tx8wL._AC_SL1500_.jpg" ,
+            nome: "Barbie O Filme Boneca Gloria Conjunto Rosa" ,
+            preco: "R$ 560,90",
+        },
+
+        barbie2: {
+            imagem: "https://m.media-amazon.com/images/I/91ZuZDF4RxL._AC_SL1500_.jpg" ,
+            nome: "Barbie O Filme, Edição Barbie Land, boneca de coleção Barbie Signature" ,
+            preco: "R$ 599,90",
+        },
+    }
+}
+
+var produtos = document.getElementById("produtos");

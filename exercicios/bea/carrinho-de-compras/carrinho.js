5112124f99252454bb75cb14d313a063f1c940e8
Matheus Bortoletto <matheus@suaquadra.com.br>
2023-08-07 19:33:50 -0300

    carreção bea

--- exercicios/bea/carrinho-de-compras/carrinho.js
+++ exercicios/bea/carrinho-de-compras/carrinho.js
@@ -1,9 +1,3 @@
-
-
-function finalizarCompra(){
-    alert(e.value);
-  }
-  
-  <input id="comprar" type="button" onclick="alertarBotao(this)" value="Parabéns! Você finalizou a sua compra com sucesso"></input>
-
- 
+function finalizarCompra() {
+  alert("Compra finalizada");
+}

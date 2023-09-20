bf0bf86b9d86ab3b5b9772d705d204370b073013
Matheus Bortoletto <matheus@suaquadra.com.br>
2023-09-20 20:28:18 -0300

    router

--- aula08-vue/src/App.vue
+++ aula08-vue/src/App.vue
@@ -1,86 +1,22 @@
 <template>
-  <div class="quiz">
-    <img src="quiz.jpg" />
-    <h3 class="pergunta">{{ quiz[quizAtual].pergunta }}</h3>
-    <h4 class="pergunta">{{ alternativa }}</h4>
-    <div class="reposta-container">
-      <AlternativaQuiz
-        @click="confere(resposta)"
-        v-for="(resposta, alternativa) in quiz[quizAtual].alternativas"
-        :key="alternativa"
-        :alternativa="alternativa"
-        @informaPai="(alternativa) => (this.alternativa = alternativa)"
-      />
-    </div>
-  </div>
+  <header class="header">
+    <h3>Central de jogos +1code</h3>
+    <router-link to="/jogo/quiz">Quiz</router-link>
+  </header>
+  <section>
+    <router-view />
+  </section>
+  <footer></footer>
 </template>
 
 <script>
-import AlternativaQuiz from "./components/AlternativaQuiz.vue";
-
 export default {
   name: "App",
-  components: { AlternativaQuiz },
-  data() {
-    return {
-      alternativa: "",
-      quizAtual: 0,
-      quiz: [
-        {
-          pergunta: "O que é um pontinho colorido no céu",
-          alternativas: {
-            Pombo: false,
-            Estrela: false,
-            Passaro: false,
-            "Padre do balão": true,
-          },
-        },
-        {
-          pergunta: "Qual é a capital da França?",
-          alternativas: {
-            Londres: false,
-            Berlim: false,
-            Paris: true,
-            Brasil: false,
-          },
-        },
-        {
-          pergunta: "Qual é o maior planeta do nosso sistema solar?",
-          alternativas: {
-            Terra: false,
-            Vênus: false,
-            Júpiter: true,
-          },
-        },
-      ],
-    };
-  },
-  props: {},
-  methods: {
-    confere(acertou) {
-      if (acertou) {
-        alert("Parabéns você acertou");
+  mounted() {
+    console.log(this.$router);
 
-        if (this.quizAtual == this.quiz.length - 1) {
-          const restart = confirm(
-            "Parabéns você é muito inteligente! Quer recomençar?"
-          );
-
-          if (restart) {
-            this.quizAtual = 0;
-          }
-        } else {
-          this.quizAtual++;
-        }
-      } else {
-        alert("Errow");
-      }
-    },
-    alert(v) {
-      alert(v);
-    },
+    console.log(this.$route);
   },
-  computed: {},
 };
 </script>
 
@@ -93,41 +29,14 @@ export default {
   color: #2c3e50;
 }
 
-.quiz {
-}
-
-.pergunta {
-  padding: 20px;
-  border-top: 1px solid purple;
+.header {
+  width: 100%;
+  font-size: 20px;
+  padding: 16px 32px;
   border-bottom: 1px solid purple;
-
-  font-size: 32px;
-  background: rgba(130, 0, 130, 0.2);
 
   color: rgb(255, 192, 32);
   text-shadow: 2px 2px 0 #000, -1px -1px 0 #000, 1px -1px 0 #000,
     -1px 1px 0 #000, 1px 1px 0 #000;
-}
-
-.resposta {
-  padding: 20px;
-  background: rgba(255, 192, 32, 0.4);
-  border-radius: 60px;
-
-  margin-left: 42px;
-  margin-right: 42px;
-
-  font-weight: bold;
-
-  cursor: pointer;
-  box-sizing: border-box;
-
-  border: 1px solid transparent;
-
-  color: #111;
-
-  &:hover {
-    border: 1px solid purple;
-  }
 }
 </style>
--- aula08-vue/src/main.js
+++ aula08-vue/src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import App from "./App.vue";
+import JogoQuiz from "./components/JogoQuiz.vue";
+import Oi from "./components/Oi.vue";
 
-createApp(App).mount('#app')
+const routes = [
+  { path: "/", name: "index", component: App },
+  { path: "/jogo/quiz", name: "quiz", component: JogoQuiz },
+  { path: "/oi", name: "oi", component: Oi },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");

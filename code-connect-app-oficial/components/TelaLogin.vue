c33444538abbf183101e180edd632831a0a5a5a6
Gabriel <gabriel.amiiller@gmail.com>
2023-10-25 23:48:50 -0300

    iniciando tela de login

--- code-connect-app-oficial/components/TelaLogin.vue
+++ code-connect-app-oficial/components/TelaLogin.vue
@@ -1,9 +1,64 @@
 <template>
-  <h1>Login</h1>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label for="email">Email:</label>
+        <input 
+          type="email" 
+          id="email"
+          @blur="validarEmail" 
+          v-model="email" 
+          required>
+      </div>
+      <span class="error-message" v-if="emailError"> {{ emailError }}</span>
+      <div>
+        <label for="password">Senha:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <button type="submit">Entrar</button>
+    </form>
+  </div>
 </template>
 <script>
 export default {
   name: "TelaLogin",
+  data() {
+    return {
+      email: "",
+      password: "",
+      emailError: null
+    }
+  },
+  methods: {
+    validarEmail() {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
+
+      if(!emailRegex.test(this.email)) {
+        this.emailError = "Formato de email inválido"
+      } else {
+        this.emailError = null
+      }
+    },
+    async login() {
+      if(this.emailError) {
+        return
+      }
+      try {
+        const response = await fetch("https://run.mocky.io/v3/25eb88e7-53e6-40cf-926f-214c6bfa83e1")
+        if(response.ok) {
+          console.log("entrou");
+        }
+      } catch(error) {
+        console.log(error);
+      }
+    }
+  }
 };
 </script>
-<style></style>
+<style>
+  .error-message {
+    color: red;
+    border: red;
+  }
+</style>

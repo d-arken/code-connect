0d2a9272a27ec497e2dbb0fdac4094daf409fc4b
windsonamorim <windson.amorim@gmail.com>
2023-10-28 00:10:43 -0300

    feat: cria components

---
+++ code-connect-app-oficial/components/button/ButtonDefault.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+   name: 'ButtonDefault'
+}
+</script>
+<template>
+    <button>teste</button>
+</template>
+<style scoped>
+    button{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-self: stretch;
+        border-radius: 30px;
+        padding: 14px 24px;
+        background: linear-gradient(271deg, #888BF4 0%, #5151C6 100%);
+        color: #ffffff;
+        text-transform: uppercase;
+        font-size: 16px;
+        font-weight: 700;
+    }
+</style>
---
+++ code-connect-app-oficial/components/input/InputDefault.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default{
+    name:'InputDefault',
+    props:{
+        placeholder: String,
+        type: String,
+        
+    }
+};
+</script>
+
+<template>
+    <div>
+        <input v-model="value" :type="type" :placeholder="placeholder"/>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        display: inline-flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+   input{
+    display: flex;
+    width: 315px;
+    padding: 13px 20px;
+    align-items: center;
+    gap: 24px;
+    justify-content: center;
+    border-radius: 30px;
+    background-color:#F3F5F7;
+   } 
+</style>
